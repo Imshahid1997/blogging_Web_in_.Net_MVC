4524bd11c183e7c9c6fcd198ad2ec4b9827624d8
Muhammad Shahid <119115381+Imshahid1997@users.noreply.github.com>
2023-02-10 16:28:53 +0500

    10feb
    10feb

---
+++ blogWeb - Copy/Migrations/20230129101823_updateProfile.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace blogWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "username",
+                table: "Tbl_Profile",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "username",
+                table: "Tbl_Profile");
+        }
+    }
+}
